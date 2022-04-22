6d563b0d09ebe7595696e93db2d9cd1a4e3fe5da
TheAwiteb <awiteb@hotmail.com>
2022-04-22 10:20:34 +0300

    🧪: Add `404 NotFound` response test

--- src/api/not_found.rs
+++ src/api/not_found.rs
@@ -5,3 +5,29 @@ use actix_web::{Responder, ResponseError};
 pub async fn not_found() -> impl Responder {
     AppError::NotFound.error_response()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::api::configuration::config_routes;
+    use crate::errors::{AppError, ErrorResponse};
+    use actix_web::{http::StatusCode, test, App};
+    use serde_json::to_string;
+
+    #[actix_web::test]
+    async fn test_not_found() {
+        let app = test::init_service(App::new().configure(config_routes)).await;
+        let req = test::TestRequest::get()
+            .uri("/api/random/endpoint")
+            .to_request();
+        let res = test::call_service(&app, req).await;
+
+        assert_eq!(res.status(), StatusCode::NOT_FOUND);
+
+        let body: ErrorResponse = test::read_body_json(res).await;
+
+        assert_eq!(
+            to_string(&body).unwrap(),
+            to_string(&ErrorResponse::from(&AppError::NotFound)).unwrap()
+        );
+    }
+}
